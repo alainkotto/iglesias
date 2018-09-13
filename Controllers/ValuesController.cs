7a25b5af6f5dd16b2a7e179314129a57fdb28662
Alain Kotto <alain.kotto@gmail.com>
2018-09-13 11:27:30 -0500

    segundo commit

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ namespace IglesiaCore.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
+        // GET api/values  ejemplo
         [HttpGet]
         public IEnumerable<string> Get()
         {
